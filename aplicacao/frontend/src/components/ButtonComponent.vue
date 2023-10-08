64b8def912cde385c783ce7743e8a552485b66cb
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2023-10-07 21:42:17 -0300

    feat: add button component

---
+++ aplicacao/frontend/src/components/ButtonComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-btn min-width="200px" block rounded="xs" size="large" class="green-button" :disabled="disable">
+        {{ text }}
+    </v-btn>
+</template>
+
+<script setup lang ="ts">
+defineProps<{
+    text: string
+    disable: boolean
+}>()
+</script>
+
+<style scoped>
+    .green-button {
+        background-color: rgba(86, 175, 119, 1);
+        color: white;
+        max-width: 100px;
+        margin-top: 3rem;
+    }
+</style>
