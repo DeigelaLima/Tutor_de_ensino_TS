d69924e930992f3a1916fd49d3be311ad1b1b9b5
Lara Souza <laragabriellysouzabatista@gmail.com>
2023-11-01 10:34:16 -0300

    removed container from homepage

--- aplicacao/frontend/src/views/HomePage.vue
+++ aplicacao/frontend/src/views/HomePage.vue
@@ -1,30 +1,27 @@
 <template>
   <v-container fluid class="home-container">
-    <v-container>
-      <v-row>
-        <!-- Coluna da esquerda para o texto -->
-        <v-col cols="12" sm="6">
-            <v-card-text class="text-left-bemVindo">
-              <p>Bem-vindo ao tutor de ensino!</p>
-            </v-card-text>
-            <v-card-text class="text-left-refatoracao">
-              <p>
-                Aqui você vai encontrar exercícios de teste de unidade<br />
-                para a refatoração de test smells.
-              </p>
-            </v-card-text>
-        </v-col>
-
-        <!-- Coluna da direita para a imagem -->
-        <v-col cols="12" sm="6">
-          <v-img
-            src="@/assets/Group6homePage.svg"
-            alt="Bem-vindo"
-            class="image-size ml-16"
-          />
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row>
+      <!-- Coluna da esquerda para o texto -->
+      <v-col cols="12" sm="6" class="text-container">
+        <v-card-text class="text-left-bemVindo">
+          <p>Bem-vindo ao tutor de ensino!</p>
+        </v-card-text>
+        <v-card-text class="text-left-refatoracao">
+          <p>
+            Aqui você vai encontrar exercícios de teste de unidade<br />
+            para a refatoração de test smells.
+          </p>
+        </v-card-text>
+      </v-col>
+      <!-- Coluna da direita para a imagem -->
+      <v-col cols="12" sm="6">
+        <v-img
+          src="@/assets/Group6homePage.svg"
+          alt="Bem-vindo"
+          class="image-size ml-16"
+        />
+      </v-col>
+    </v-row>
 
     <v-col class="choice">
       <p>Escolha um tipo de Test Smell para começar:</p>
@@ -82,6 +79,10 @@ const typesOfTestSmells = ref<TestSmell[]>([
 /* imagem homePage */
 .image-size {
   padding: 320px;
+}
+
+.text-container {
+  margin-top: 100px;
 }
 
 /* Botões */
