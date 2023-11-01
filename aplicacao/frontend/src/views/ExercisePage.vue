e83a30182b303c0c7f69b69d27b0da1d5db8cfe1
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2023-11-01 14:53:05 -0300

    refactor: add title, code and link example

--- aplicacao/frontend/src/views/ExercisePage.vue
+++ aplicacao/frontend/src/views/ExercisePage.vue
@@ -3,7 +3,7 @@
         <v-row>
             <v-col>
                 <h1 align="center">{{ exerciseTitle }}</h1>
-                <Codemirror style="height: 60vh; width: 50vw;" />
+                <Codemirror :model-value="exercise" style="height: 60vh; width: 50vw;" />
                 <h5>{{ exerciseLink }}</h5>
             </v-col>
             <v-col>
@@ -27,21 +27,26 @@ import { ref } from 'vue';
 import { Codemirror } from 'vue-codemirror';
 import ButtonComponent from '../components/ButtonComponent.vue';
 
-const smellDescription = ref("Esse tipo de Test Smell ocorre quando um método de teste é suprimido de execução "+
-                             "usando recursos fornecidos pelo JUnit 4. No entanto, essa abordagem de Ignored "+
-                             "Test pode resultar em sobrecarga durante o tempo de compilação, além de aumentar "+
-                             "a complexidade e dificultar a compreensão do código.");   
-const smellName = ref("Ignored Test")
-const exerciseTitle = ref("Teste")
-const exerciseLink = ref("Exercise repo link")
+const smellDescription = ref("Esse tipo de Test Smell ocorre quando um método de teste é suprimido de execução usando recursos fornecidos pelo JUnit 4. No entanto, essa abordagem de Ignored Test pode resultar em sobrecarga durante o tempo de compilação, além de aumentar a complexidade e dificultar a compreensão do código.");   
+const smellName = ref("Ignored Test");
+const exerciseTitle = ref("Teste");
+const exerciseLink = ref("Exercise repo link");
+const exercise = ref(`@Ignore\n@Test\npublic void testSampling() {\n      final double[] weights = { 0.3, 0.7 };\n      final double[][] means = { { -1.5, 2.0 }, { 4.0, 8.2 } };\n      final double[][][] covariances = { { { 2.0, -1.1 },{ -1.1, 2.0 } },{ { 3.5, 1.5 },\n{ 1.5, 3.5 } } };\n      final MixtureMultivariateNormalDistribution d = \n          new MixtureMultivariateNormalDistribution(weights, means, covariances);\n      final MultivariateRealDistribution.Sampler sampler =\n          d.createSampler(RandomSource.WELL_19937_C.create(50));\n      final double[][] correctSamples = getCorrectSamples();\n      final int n = correctSamples.length;\n      final double[][] samples=AbstractMultivariateRealDistribution.sample(n,sampler);\n       for (int i = 0; i < n; i++) {\n           for (int j = 0; j < samples[i].length; j++) {\n                Assert.assertEquals('sample[" + j + "]", \n                  correctSamples[i][j], samples[i][j], 1e-16);\n
+           }\n
+       }\n
+}
+`);
+
 </script>
 
 <style scoped>
+
 
     .container {
         margin-top: 30px;
         height: 100vh;
     }
+
     .foot-buttons{
         padding-top: 2rem;
     }
