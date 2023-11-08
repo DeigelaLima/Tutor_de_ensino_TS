228060f0d601f077bfd94243a33580c226da81e3
Victor Anthony <anthonyvictor90@gmail.com>
2023-11-08 15:06:09 -0300

    resolved cors problems

--- aplicacao/backend/src/routes/tsRoutes.js
+++ aplicacao/backend/src/routes/tsRoutes.js
@@ -1,9 +1,28 @@
 const { Router } = require("express");
+const cors = require("cors");
 const TsController = require("../app/controllers/TsController");
 const bodyParser = require('body-parser')
 const typeBody =  bodyParser.text({type:"*/*"})
 
 const tsRouter = Router();
+
+const whitelist = [
+    'http://localhost:8080',
+    'http://localhost:3000',
+  ];
+  
+  const corsOptions = {
+    origin(origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+
+
+tsRouter.use(cors(corsOptions));
 
 tsRouter.get('/exercises', TsController.getAllExercises)
 tsRouter.get('/exercises/:id', TsController.getExerciseByCategoryId)
