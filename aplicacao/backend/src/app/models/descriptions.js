d25938ddad11839f476ab74c719b80d6504792df
Deigela_Lima <deigelalima@hotmail.com>
2023-11-18 22:09:51 -0300

    Editing the descriptions of the smells.

--- aplicacao/backend/src/app/models/descriptions.js
+++ aplicacao/backend/src/app/models/descriptions.js
@@ -1,16 +1,81 @@
 const descriptions = {
-    de1: "O teste testSampling faz parte do código de produção que define a classe MixtureMultivariateNormalDistribution. O código de produção estabelece uma classe para modelar uma distribuição de mistura de distribuições normais multivariadas, enquanto o código de teste verifica o comportamento dessa classe em diferentes cenários e assegura que ela está funcionando conforme o esperado. O teste verifica se a distribuição é capaz de gerar amostras que são aproximadamente iguais aos valores corretos, o que é crucial para garantir a precisão da implementação da distribuição. Se os valores amostrados estiverem dentro da tolerância especificada, o teste é considerado bem-sucedido.",
-    de2: "O teste testRandomUnsymmetricMatrix faz parte do código de produção que define a classe EigenDecomposition, usada para calcular a decomposição de autovalores de uma matriz real. Este teste automatizado executa a decomposição espectral em matrizes não simétricas geradas aleatoriamente várias vezes para verificar a robustez e correção da implementação em diversos cenários. O código de teste verifica se essa implementação está funcionando corretamente e produzindo resultados precisos em diferentes situações, assegurando a qualidade e confiabilidade da implementação.",
-    de3: "O teste testNormalDistributionUnsymmetricMatrix faz parte do código de produção que define a classe EigenDecomposition, utilizada para calcular a decomposição de autovalores de uma matriz real. Este código de teste, denominado testNormalDistributionUnsymmetricMatrix, tem o objetivo de testar a decomposição espectral em matrizes não simétricas geradas a partir de amostras de uma distribuição normal de números aleatórios. O código de teste verifica se essa implementação está funcionando corretamente e produzindo resultados precisos em uma variedade de cenários, garantindo a qualidade e a confiabilidade da implementação.",
-    de4: "O teste testConstructor0 faz parte do código de produção que define a classe EncoderException, uma exceção para representar erros de codificação. Esse teste avalia o comportamento do construtor padrão da EncoderException, assegurando que a instância gerada não contenha mensagem de detalhes (null) e não possua causa (null), conforme a expectativa. A validação do construtor é crucial para garantir a integridade e confiabilidade da biblioteca Apache Commons Codec.",
-    de5: "O teste testConstructorString faz parte do código de produção que define a classe EncoderException, uma exceção para representar erros específicos relacionados à codificação. Este método avalia o comportamento do construtor da EncoderException, que recebe uma mensagem de detalhes como argumento. O teste verifica se esse construtor cria uma instância com a mensagem correta e sem causa (que é null), de acordo com o comportamento esperado. O sucesso do teste é determinado pela aprovação das verificações assertEquals e assertNull. Esses testes são essenciais para garantir a integridade e confiabilidade dos construtores da biblioteca Apache Commons Codec.",
-    de6: "O método testConstructorThrowable() testa se o construtor da classe EncoderException, que recebe uma causa (outra exceção) como argumento, cria uma instância com a mensagem de detalhes configurada como o nome da classe da causa e com a causa corretamente definida. Se ambas as verificações assertEquals passarem, o teste é bem-sucedido",
-    de7: "O teste testEncodeHexByteArrayHelloWorldLowerCaseHex faz parte do código de produção Hex, abordando a funcionalidade real de codificação e decodificação em formato hexadecimal. O código de teste verifica a implementação correta dessa funcionalidade em diversas situações, garantindo a robustez e confiabilidade da classe Hex em diferentes contextos de uso. O foco do teste está na codificação de uma sequência de bytes em representação hexadecimal, especialmente na variação entre letras maiúsculas e minúsculas na saída. O teste assegura que a classe Hex codifique arrays de bytes em representações hexadecimais, respeitando as opções de codificação em letras maiúsculas e minúsculas.",
-    de8: "O teste testEncodeDecodeBlanks faz parte do código de produção da biblioteca Apache Commons Codec, implementando a classe QCodec responsável pela codificação e decodificação de strings no formato 'quoted-printable', conforme a norma RFC 1522. Este formato é amplamente utilizado para representar dados binários de maneira segura em e-mails, permitindo apenas caracteres imprimíveis. O teste verifica se a classe QCodec manipula corretamente espaços em branco, mantendo-os intactos ou substituindo-os por sublinhados, conforme especificado pela RFC 1522. Essa verificação garante o comportamento preciso da classe ao lidar com espaços em branco durante a codificação e decodificação de strings",
-    de9: "O teste testDecodeObjects pertence ao código de produção da classe QuotedPrintableCodec no pacote org.apache.commons.codec.net, parte integrante da biblioteca Apache Commons Codec. Utilizada para codificar e decodificar dados no formato Quoted-Printable, a classe é capaz de decodificar objetos representando dados nesse formato. O teste confirma se a classe consegue decodificar corretamente strings e arrays de bytes nesse formato, assegurando que exceções sejam lançadas ao tentar decodificar objetos fora do formato correto. Isso garante o funcionamento adequado da classe em diversas situações.",
-    de10: "O teste testUTF8RoundTrip faz parte do código de produção BCodec da biblioteca Apache Commons Codec, usado para codificar e decodificar dados em Base64 conforme o RFC 1521. Esse teste assegura que o BCodec manipula corretamente caracteres especiais em UTF-8, garantindo sua robustez em aplicações reais. A funcionalidade de ida e volta do BCodec é verificada para garantir consistência entre mensagens codificadas e decodificadas. Essa validação é crucial para cenários do mundo real que envolvem a codificação de texto com caracteres não-ASCII.",
-    de11: "O teste testDefaultEncoding faz parte do código de produção da classe URLCodec, responsável pela codificação e decodificação de URLs. O código de teste verifica a precisão dessa funcionalidade, abrangendo casos diversos e excepcionais. Especificamente, o teste avalia a correta codificação de URLs com um conjunto personalizado de caracteres ('UnicodeBig') e verifica a consistência com a codificação padrão. O método validateState é utilizado para garantir a manutenção adequada do estado interno da classe após os testes, assegurando o correto funcionamento em diferentes situações.",
-    de12: "O teste testDecodeInvalidContent integra o código de produção da classe URLCodec, sendo responsável por verificar a capacidade dessa classe em decodificar corretamente conteúdos codificados em ISO-8859-1. O objetivo é preservar tanto o tamanho quanto os bytes originais do conteúdo, assegurando que a classe possa manipular distintos conjuntos de caracteres e codificações de maneira apropriada. O método validateState é empregado para garantir a adequada manutenção do estado interno da classe após a execução dos testes, assegurando o correto funcionamento em diferentes cenários.",
+    de1:
+    "O teste testSampling faz parte do código de produção que define a classe MixtureMultivariateNormalDistribution. "+
+    "O código de produção estabelece uma classe para modelar uma distribuição de mistura de distribuições normais multivariadas, "+
+    "enquanto o código de teste verifica o comportamento dessa classe em diferentes cenários e assegura que ela está funcionando conforme o esperado. "+
+    "O teste verifica se a distribuição é capaz de gerar amostras que são aproximadamente iguais aos valores corretos, "+
+    "o que é crucial para garantir a precisão da implementação da distribuição. Se os valores amostrados estiverem dentro da tolerância especificada, "+
+    "o teste é considerado bem-sucedido.",
+    de2: 
+    "O teste testRandomUnsymmetricMatrix faz parte do código de produção que define a classe EigenDecomposition, "+
+    "usada para calcular a decomposição de autovalores de uma matriz real. Este teste automatizado executa a decomposição espectral em matrizes não simétricas "+
+    "geradas aleatoriamente várias vezes para verificar a robustez e correção da implementação em diversos cenários. "+
+    "O código de teste verifica se essa implementação está funcionando corretamente e produzindo resultados precisos em diferentes situações, "+
+    "assegurando a qualidade e confiabilidade da implementação.",
+    de3: 
+    "O teste testNormalDistributionUnsymmetricMatrix faz parte do código de produção que define a classe EigenDecomposition, "+
+    "utilizada para calcular a decomposição de autovalores de uma matriz real. Este código de teste, denominado testNormalDistributionUnsymmetricMatrix, "+
+    "tem o objetivo de testar a decomposição espectral em matrizes não simétricas geradas a partir de amostras de uma distribuição normal de números aleatórios. "+
+    "O código de teste verifica se essa implementação está funcionando corretamente e produzindo resultados precisos em uma variedade de cenários, "+
+    "garantindo a qualidade e a confiabilidade da implementação.",
+    de4: 
+    "O teste testConstructor0 faz parte do código de produção que define a classe EncoderException, "+
+    "uma exceção para representar erros de codificação. Esse teste avalia o comportamento do construtor padrão da EncoderException, "+
+    "assegurando que a instância gerada não contenha mensagem de detalhes (null) e não possua causa (null), conforme a expectativa. "+
+    "A validação do construtor é crucial para garantir a integridade e confiabilidade da biblioteca Apache Commons Codec.",
+    de5: 
+    "O teste testConstructorString faz parte do código de produção que define a classe EncoderException, "+
+    "uma exceção para representar erros específicos relacionados à codificação. Este método avalia o comportamento do construtor da EncoderException, "+
+    "que recebe uma mensagem de detalhes como argumento. O teste verifica se esse construtor cria uma instância com a mensagem correta e sem causa (que é null), "+
+    "de acordo com o comportamento esperado. O sucesso do teste é determinado pela aprovação das verificações assertEquals e assertNull. "+
+    "Esses testes são essenciais para garantir a integridade e confiabilidade dos construtores da biblioteca Apache Commons Codec.",
+    de6: 
+    "O método testConstructorThrowable() testa se o construtor da classe EncoderException, que recebe uma causa (outra exceção) como argumento, "+
+    "cria uma instância com a mensagem de detalhes configurada como o nome da classe da causa e com a causa corretamente definida. "+
+    "Se ambas as verificações assertEquals passarem, o teste é bem-sucedido",
+    de7: 
+    "O teste testEncodeHexByteArrayHelloWorldLowerCaseHex faz parte do código de produção Hex, "+
+    "abordando a funcionalidade real de codificação e decodificação em formato hexadecimal. "+
+    "O código de teste verifica a implementação correta dessa funcionalidade em diversas situações, "+
+    "garantindo a robustez e confiabilidade da classe Hex em diferentes contextos de uso. "+
+    "O foco do teste está na codificação de uma sequência de bytes em representação hexadecimal, "+
+    "especialmente na variação entre letras maiúsculas e minúsculas na saída. "+
+    "O teste assegura que a classe Hex codifique arrays de bytes em representações hexadecimais, "+
+    "respeitando as opções de codificação em letras maiúsculas e minúsculas.",
+    de8: 
+    "O teste testEncodeDecodeBlanks faz parte do código de produção da biblioteca Apache Commons Codec, "+
+    "implementando a classe QCodec responsável pela codificação e decodificação de strings no formato 'quoted-printable', "+
+    "conforme a norma RFC 1522. Este formato é amplamente utilizado para representar dados binários de maneira segura em e-mails, "+
+    "permitindo apenas caracteres imprimíveis. O teste verifica se a classe QCodec manipula corretamente espaços em branco, "+
+    "mantendo-os intactos ou substituindo-os por sublinhados, conforme especificado pela RFC 1522. "+
+    "Essa verificação garante o comportamento preciso da classe ao lidar com espaços em branco durante a codificação e decodificação de strings",
+    de9: 
+    "O teste testDecodeObjects pertence ao código de produção da classe QuotedPrintableCodec no pacote org.apache.commons.codec.net, "+
+    "parte integrante da biblioteca Apache Commons Codec. Utilizada para codificar e decodificar dados no formato Quoted-Printable, "+
+    "a classe é capaz de decodificar objetos representando dados nesse formato. O teste confirma se a classe consegue decodificar corretamente strings "+
+    "e arrays de bytes nesse formato, assegurando que exceções sejam lançadas ao tentar decodificar objetos fora do formato correto. "+
+    "Isso garante o funcionamento adequado da classe em diversas situações.",
+    de10:
+    "O teste testUTF8RoundTrip faz parte do código de produção BCodec da biblioteca Apache Commons Codec, "+
+    "usado para codificar e decodificar dados em Base64 conforme o RFC 1521. "+
+    "Esse teste assegura que o BCodec manipula corretamente caracteres especiais em UTF-8, "+
+    "garantindo sua robustez em aplicações reais. A funcionalidade de ida e volta do BCodec é verificada "+
+    "para garantir consistência entre mensagens codificadas e decodificadas. "+
+    "Essa validação é crucial para cenários do mundo real que envolvem a codificação de texto com caracteres não-ASCII.",
+    de11: 
+    "O teste testDefaultEncoding faz parte do código de produção da classe URLCodec, "+
+    "responsável pela codificação e decodificação de URLs. O código de teste verifica a precisão dessa funcionalidade, "+
+    "abrangendo casos diversos e excepcionais. Especificamente, o teste avalia a correta codificação de URLs "+
+    "com um conjunto personalizado de caracteres ('UnicodeBig') e verifica a consistência com a codificação padrão. "+
+    "O método validateState é utilizado para garantir a manutenção adequada do estado interno da classe após os testes, "+
+    "assegurando o correto funcionamento em diferentes situações.",
+    de12: 
+    "O teste testDecodeInvalidContent integra o código de produção da classe URLCodec, "+
+    "sendo responsável por verificar a capacidade dessa classe em decodificar corretamente conteúdos codificados em ISO-8859-1. "+
+    "O objetivo é preservar tanto o tamanho quanto os bytes originais do conteúdo, "+
+    "assegurando que a classe possa manipular distintos conjuntos de caracteres e codificações de maneira apropriada. "+
+    "O método validateState é empregado para garantir a adequada manutenção do estado interno da classe após a execução dos testes, "+
+    "assegurando o correto funcionamento em diferentes cenários.",
 }
 
 module.exports = descriptions
--- aplicacao/frontend/src/testSmellsTypes.ts
+++ aplicacao/frontend/src/testSmellsTypes.ts
@@ -1,15 +1,42 @@
 export const TestSmells = [
-    { id: 1, name: "Assertion Roulette", description: "O Assertion Roulette ocorre quando um único teste tenta verificar múltiplos comportamentos ou resultados diferentes, tornando-o difícil de entender e manter. Isso pode dificultar a identificação da causa de uma falha no teste, pois várias asserções são executadas antes de o teste ser interrompido em caso de falha.", exercise: { idSmell: [10, 11, 12] } },
-    { id: 2, name: "Duplication Assert", description: "e refere à repetição excessiva de asserções (assertions) semelhantes em vários testes unitários. Isso significa que múltiplos testes estão verificando o mesmo comportamento ou resultado, resultando em redundância no código de teste.", exercise: { idSmell: [7, 8, 9] } },
+    { 
+        id: 1, 
+        name: "Assertion Roulette", 
+        description: "Ocorre quando um método de teste possui várias assertions sem explicação, "+
+        "dificultando a identificação da causa de um erro em caso de falha. "+
+        "Para refatorar, deve-se incluir mensagens explicativas como argumentos opcionais nas assertions, "+
+        "uma prática recomendada para melhorar a compreensão dos testes, esclarecendo o propósito de cada assertion "+
+        "e facilitando a manutenção do código. A falta dessa documentação adequada pode complicar a identificação precisa do assertion "+
+        "responsável por uma falha, tornando a depuração mais desafiadora durante a manutenção do código.",
+        exercise: { idSmell: [10, 11, 12] }
+    },
+    { 
+        id: 2,
+        name: "Duplication Assert", 
+        description: "Ocorre quando um método de teste repete a mesma condição várias vezes dentro do próprio método. "+
+        "Para evitar isso, é aconselhável refatorar, criando um novo método de teste se a mesma condição precisa ser verificada com valores diferentes. "+
+        "Isso contribui para a organização do código e evita redundâncias.",
+        exercise: { idSmell: [7, 8, 9] }
+    },
     {
-        id: 3, name: "Eager Test", description: "Um teste eager geralmente ocorre quando um desenvolvedor escreve testes que verificam um grande número de resultados ou cenários, incluindo aqueles que não têm relevância direta para o comportamento ou a funcionalidade que está sendo testada.", exercise: {
-            idSmell: [4, 5, 6]
-        }
+        id: 3, 
+        name: "Eager Test", 
+        description: "Ocorre quando um método de teste verifica diversos métodos do objeto que está sendo testado. "+
+        "Esses testes são de difícil compreensão e leitura, tornando-se menos eficazes como documentação. "+
+        "Para solucionar essa situação, recomenda-se refatorar, extraindo o método de teste e separando os assertions em métodos de teste distintos. "+
+        "Além disso, eles tendem a criar dependências entre os testes, o que torna o processo de manutenção mais desafiador e complexo.",
+        exercise: {idSmell: [4, 5, 6]}
     },
     {
         id: 4,
         name: "Ignored Test",
-        description:
-            "Esse tipo de Test Smell ocorre quando um método de teste é suprimido de execução usando recursos fornecidos pelo JUnit 4. No entanto, essa abordagem de Ignored Test pode resultar em sobrecarga durante o tempo de compilação, além de aumentar a complexidade e dificultar a compreensão do código.  ", exercise: { idSmell: [1, 2, 3] }
+        description: "Ocorre quando um método de teste é suprimido da execução. "+
+        "Contudo, essa abordagem de Ignored Test pode resultar em sobrecarga durante o tempo de compilação, "+
+        "além de aumentar a complexidade e dificultar a compreensão do código. "+
+        "Ao marcar métodos de teste com @Ignore(), há o risco de deixar lacunas na cobertura dos testes, "+
+        "pois partes cruciais do código podem não ser adequadamente verificadas. "+
+        "A refatoração do Ignored Test consiste, primeiramente, em remover a anotação @Ignore(). "+
+        "Em seguida, é necessário analisar se as instruções presentes no método de teste são relevantes e aplicáveis.",
+        exercise: { idSmell: [1, 2, 3] }
     },
 ];
