2629a299b95f113580cd1a5b027df739d737cc63
Lara Souza <laragabriellysouzabatista@gmail.com>
2023-11-24 14:05:05 -0300

    changed baseURL

--- aplicacao/frontend/src/services/ExerciseService.ts
+++ aplicacao/frontend/src/services/ExerciseService.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosResponse } from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://tutor-de-ensino-ts.onrender.com',
     timeout: 10000,
 });
 
